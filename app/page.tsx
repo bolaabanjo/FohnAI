85b7b926bf330f0f5ea642fef0ae9eb1dda37b00
bolaabanjo <royadebanjo@gmail.com>
2025-11-18 20:00:49 +0100

    Updated page footer

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-4xl flex-col justify-between py-48 px-8 bg-white dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-4xl flex-col justify-between pt-48 pb-8 px-8 bg-white dark:bg-black">
         <div className="flex flex-col gap-8 max-w-3xl">
           <div className="prose dark:prose-invert max-w-none">
             <p className="text-lg leading-relaxed text-zinc-700 dark:text-zinc-300">
